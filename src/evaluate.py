1785ff7f1b7359b98f78db697d2dab6b9ba00249
megrsvp <40273817+megrsvp@users.noreply.github.com>
2025-01-02 10:41:16 -0500

    deleted unnecessary files

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,134 +1,45 @@
-# Add src to Python's path
+import torch
+import pytorch_lightning as pl
+import torch.nn.functional as F
+from torch.optim import Adam
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
+import os
 import sys
+# Append custom source directories to sys.path
 sys.path.append('./src')
 sys.path.append('./src/utils')
 
-import os
-from video_dataset import VideoDataset
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import DataLoader, random_split
-import models
-from models import ResNet3D
-import torch.optim as optim
-from tqdm import tqdm
 from loss import loss_function
-from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
+from models_MiT import load_model, MultiHeadLinear  # Adapt to your local imports
+from utils.training_utils import load_checkpoint
+from data import get_dataloaders
+from config import config  # Import the config dictionary
 
-# Load checkpoint (Optional)
-def load_checkpoint(checkpoint_path):
-    model.load_state_dict(torch.load(checkpoint_path))
-    return model
+train_loader, val_loader = get_dataloaders(config)
 
+checkpoint_path = os.path.join(os.getcwd(),'model_checkpoints','fine_tuned','model_epoch=6_val_loss=0.0211.ckpt')
 
-# 1. Set up configurations (optional config file)
-config = {
-    "num_epochs": 200,
-    "learning_rate": 0.001,
-    "batch_size": 16,
-    "print_interval": 400,
-    "alpha": 1.0,
-    "beta": 1.0,
-    "num_workers": 1,
-    "reduce_dataset": True
-}
+# Load 3D ResNet model
+model = load_model('multi_resnet3d50')  
+model.last_linear = MultiHeadLinear(in_features=2048)
+load_checkpoint(model, checkpoint_path)
+model.to('cuda')
 
-# Use config settings in your code
-num_epochs = config["num_epochs"]
-learning_rate = config["learning_rate"]
-batch_size = config["batch_size"]
-print_interval = config["print_interval"]
-alpha = config["alpha"]
-beta = config["beta"]
-num_workers = config["num_workers"]
-
-
-# 2. Load dataset and DataLoader
-video_dir = [r'H:\tmp\processed_videos']
-           
-
-labels_csv = os.path.join(os.getcwd(), 'data','annotations','video_annotations_combined_43392.csv')
-dataset =  VideoDataset(video_dir, labels_csv)
-
-# Set the split ratio
-train_ratio = 0.9
-train_size = int(train_ratio * len(dataset))
-val_size = len(dataset) - train_size
-
-# Split the dataset
-generator = torch.Generator().manual_seed(42)
-train_dataset, val_dataset = random_split(dataset, [train_size, val_size], generator=generator)
-
-# Set reduce the dataset size for testing
-if config["reduce_dataset"]:
-    train_size = 2000
-    val_size = 1000
-    train_dataset = torch.utils.data.Subset(train_dataset, list(range(train_size)))
-    val_dataset = torch.utils.data.Subset(val_dataset, list(range(val_size)))
-
-# Define DataLoaders for training and validation
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
-
-
-
-
-
-# Initialize device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# 3. Initialize the model
-model = models.load_model('multi_resnet3d50')
-model.last_linear = models.MultiHeadLinear(in_features=2048) # Adjust final layer
-
-# Move model to the device
-model = model.to(device)
-
-
-
-model_path = os.path.join(os.getcwd(), 'model_checkpoints','fine_tuned','reduced_dataset_2000_fulltrained','model_checkpoint_epoch_200.pt')
-model = load_checkpoint(model_path)
-
-
-
-model.eval()
-val_loss = 0.0
-with torch.no_grad():
-    for inputs, labels in tqdm(train_loader):
-        inputs, labels = inputs.to(device), {k: v.to(device) for k, v in labels.items()}  # Move to device
-        outputs = model(inputs)
-        loss = loss_function(outputs, labels)
-        val_loss += loss.item()
-
-
-
-
-# Load checkpoint (Optional)
-def load_checkpoint(checkpoint_path):
-    model.load_state_dict(torch.load(checkpoint_path))
-    return model
-
-
-
-
-
-checkpoint_path = 'model_checkpoint_epoch_7.pt'
-model = load_checkpoint(checkpoint_path)
 
 
 myiter = iter(train_loader)
 inputs, labels = next(myiter)
+device = 0
 inputs, labels = inputs.to(device), {k: v.to(device) for k, v in labels.items()}  # Move to device
 outputs = model(inputs)
 
-l = loss = loss_function(outputs, labels, alpha, beta, include_confidence)  # Calculate loss
-print(l)
+loss = loss_function(outputs, labels, alpha=0, beta=0, include_confidence=False, train_tasks = 'arousal')  # Calculate loss
+print(loss)
 
-question_type = 'expanse'
-target = labels[f'{question_type}_score'].float()
+task = 'arousal'
+target = labels[f'{task}_score'].float()
 #pred = torch.sigmoid(outputs[f'{question_type}_score']).flatten()
-pred = outputs[f'{question_type}_score']
+pred = outputs[f'{task}_score']
 target = target.unsqueeze(1)
 #pred = pred.unsqueeze(1)
 pred = torch.sigmoid(pred)
